b128015dd75de9a99fce40f7443921b08e8eebd6
Sebastian Galvan <sebastiangalvan.ar@gmail.com>
2024-01-21 03:00:51 -0300

    [UPDATE] - Imports no usados

--- src/main/java/com/jasperreports/sprigboot/Reportes/repository/EmpleadoRepository.java
+++ src/main/java/com/jasperreports/sprigboot/Reportes/repository/EmpleadoRepository.java
@@ -4,9 +4,6 @@ import com.jasperreports.sprigboot.Reportes.entity.Empleado;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Repository
 public interface EmpleadoRepository extends JpaRepository<Empleado, Long> {
 
